d992a4f86bd41b828dccd5010e88e4b60a1de4d7
Rachael Rice <Rachnicrice@gmail.com>
2019-08-23 08:52:38 -0700

    Added CSS styling and started to parse together the logic

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -3,15 +3,19 @@
 // Cart constructor.
 var Cart = function(items) {
   // this.items is an array of CartItem instances.
-  this.items = items;
+  this.items = [items];
 };
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  var newItem = new CartItem (product, quantity);
+  Cart.push(newItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  var cart = JSON.stringify(Cart.items);
+  localStorage.setItem(cart);
 };
 
 Cart.prototype.removeItem = function(item) {
@@ -33,26 +37,26 @@ var Product = function(filePath, name) {
 Product.allProducts = [];
 
 function generateCatalog() {
-  new Product('assets/bag.jpg', 'Bag');
-  new Product('assets/banana.jpg', 'Banana');
-  new Product('assets/bathroom.jpg', 'Bathroom');
-  new Product('assets/boots.jpg', 'Boots');
-  new Product('assets/breakfast.jpg', 'Breakfast');
-  new Product('assets/bubblegum.jpg', 'Bubblegum');
-  new Product('assets/chair.jpg', 'Chair');
-  new Product('assets/cthulhu.jpg', 'Cthulhu');
-  new Product('assets/dog-duck.jpg', 'Dog-Duck');
-  new Product('assets/dragon.jpg', 'Dragon');
-  new Product('assets/pen.jpg', 'Pen');
-  new Product('assets/pet-sweep.jpg', 'Pet Sweep');
-  new Product('assets/scissors.jpg', 'Scissors');
-  new Product('assets/shark.jpg', 'Shark');
-  new Product('assets/sweep.png', 'Sweep');
-  new Product('assets/tauntaun.jpg', 'Taun-Taun');
-  new Product('assets/unicorn.jpg', 'Unicorn');
-  new Product('assets/usb.gif', 'USB');
-  new Product('assets/water-can.jpg', 'Water Can');
-  new Product('assets/wine-glass.jpg', 'Wine Glass');
+  new Product('../assets/bag.jpg', 'Bag');
+  new Product('../assets/banana.jpg', 'Banana');
+  new Product('../assets/bathroom.jpg', 'Bathroom');
+  new Product('../assets/boots.jpg', 'Boots');
+  new Product('../assets/breakfast.jpg', 'Breakfast');
+  new Product('../assets/bubblegum.jpg', 'Bubblegum');
+  new Product('../assets/chair.jpg', 'Chair');
+  new Product('../assets/cthulhu.jpg', 'Cthulhu');
+  new Product('../assets/dog-duck.jpg', 'Dog-Duck');
+  new Product('../assets/dragon.jpg', 'Dragon');
+  new Product('../assets/pen.jpg', 'Pen');
+  new Product('../assets/pet-sweep.jpg', 'Pet Sweep');
+  new Product('../assets/scissors.jpg', 'Scissors');
+  new Product('../assets/shark.jpg', 'Shark');
+  new Product('../assets/sweep.png', 'Sweep');
+  new Product('../assets/tauntaun.jpg', 'Taun-Taun');
+  new Product('../assets/unicorn.jpg', 'Unicorn');
+  new Product('../assets/usb.gif', 'USB');
+  new Product('../assets/water-can.jpg', 'Water Can');
+  new Product('../assets/wine-glass.jpg', 'Wine Glass');
 }
 
 // Initialize the app by creating the big list of products with images and names
--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -11,10 +11,12 @@ function populateForm() {
 
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
-  for (var i in Product.allProducts) {
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    var option = document.createElement('option');
+    option.textContent = Product.allProducts[i].name;
 
+    selectElement.appendChild(option);
   }
-
 }
 
 // When someone submits the form, we need to add the selected item to the cart
@@ -23,29 +25,57 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
-  addSelectedItemToCart();
+  addSelectedItemToCart(event);
   cart.saveToLocalStorage();
   updateCounter();
-  updateCartPreview();
+  updateCartPreview(event);
 
 }
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  var itemSelected = event.target.items.value;
   // TODO: get the quantity
+  var itemQuantity = event.target.quantity.value;
   // TODO: using those, add one item to the Cart
+  Cart.addItem(itemSelected, itemQuantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  var cartCounter = document.getElementById('itemCount');
+  cartCounter.textContent = `${Cart.items.length}`;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
-function updateCartPreview() {
+function updateCartPreview(event) {
   // TODO: Get the item and quantity from the form
+  var itemSelected = event.target.items.value;
+  var itemQuantity = event.target.quantity.value;
+
   // TODO: Add a new element to the cartContents div with that information
+  var cartContents = document.getElementById('cart');
+  var newRow = document.createElement('tr');
+  newRow.id = itemSelected;
+  cartContents.appendChild(newRow);
+
+  var addToRow = document.getElementById(itemSelected);
+  var removeButton = document.createElement('td');
+  removeButton.textContent = 'X';
+  addToRow.appendChild('removeButton');
+
+  var quantity = document.createElement('td');
+  quantity.textContent = itemQuantity;
+  addToRow.appendChild(quantity);
+
+  var item = document.createElement('td');
+  item.textContent = itemSelected;
+  addToRow.appendChild(item);
+
 }
 
 // Set up the "submit" event listener on the form.
